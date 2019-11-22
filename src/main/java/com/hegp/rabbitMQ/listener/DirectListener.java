3fc6e08f3b62006c6dc0c09ea6b8140b6a9c0036
heguopei <heguopei@ascs.tech>
2019-11-22 18:38:56 +0800

    添加完整参数的代码例子

--- src/main/java/com/hegp/rabbitMQ/listener/DirectListener.java
+++ src/main/java/com/hegp/rabbitMQ/listener/DirectListener.java
@@ -1,19 +1,50 @@
 package com.hegp.rabbitMQ.listener;
 
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import com.hegp.po.Mail;
 
+import java.io.IOException;
+import java.util.Map;
+
 @Component
 public class DirectListener {
 	@RabbitListener(queues = "directqueue1")
-	public void displayMail1(Mail mail) {
+	public void displayMail1(Message message, @Headers Map<String, Object> headers, Channel channel, @Payload Mail mail) {
+		System.out.println(headers);
+		try {
+			System.out.println(message);
+			//告诉服务器收到这条消息 已经被我消费了 可以在队列删掉 这样以后就不会再发了 否则消息服务器以为这条消息没处理掉 后续还会在发
+			channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+			System.out.println("receiver success");
+		} catch (IOException e) {
+			e.printStackTrace();
+			//丢弃这条消息
+			//channel.basicNack(message.getMessageProperties().getDeliveryTag(), false,false);
+			System.out.println("receiver fail");
+		}
 		System.out.println("directqueue1队列监听器1号收到消息"+mail.toString());
 	}
 
 	@RabbitListener(queues = "directqueue2")
-	public void displayMail2(Mail mail) {
+	public void displayMail2(Message message, @Headers Map<String, Object> headers, Channel channel, @Payload Mail mail) {
+		System.out.println(headers);
+		try {
+			System.out.println(message);
+			//告诉服务器收到这条消息 已经被我消费了 可以在队列删掉 这样以后就不会再发了 否则消息服务器以为这条消息没处理掉 后续还会在发
+			channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+			System.out.println("receiver success");
+		} catch (IOException e) {
+			e.printStackTrace();
+			//丢弃这条消息
+			//channel.basicNack(message.getMessageProperties().getDeliveryTag(), false,false);
+			System.out.println("receiver fail");
+		}
 		System.out.println("directqueue2队列监听器2号收到消息"+mail.toString());
 	}
 }
--- src/main/java/com/hegp/rabbitMQ/listener/TopicListener.java
+++ src/main/java/com/hegp/rabbitMQ/listener/TopicListener.java
@@ -2,12 +2,16 @@ package com.hegp.rabbitMQ.listener;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import com.hegp.po.Mail;
 
+import java.io.IOException;
 import java.util.Map;
 
 @Component
@@ -15,13 +19,36 @@ public class TopicListener {
 	private ObjectMapper mapper = new ObjectMapper();
 
 	@RabbitListener(queues = "topicqueue1")
-	public void displayTopic1(Mail mail) {
+	public void displayTopic1(Message message, @Headers Map<String, Object> headers, Channel channel, @Payload Mail mail) {
+		System.out.println(headers);
+		try {
+			System.out.println(message);
+			//告诉服务器收到这条消息 已经被我消费了 可以在队列删掉 这样以后就不会再发了 否则消息服务器以为这条消息没处理掉 后续还会在发
+			channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+			System.out.println("receiver success");
+		} catch (IOException e) {
+			e.printStackTrace();
+			//丢弃这条消息
+			//channel.basicNack(message.getMessageProperties().getDeliveryTag(), false,false);
+			System.out.println("receiver fail");
+		}
 		System.out.println("从topicqueue1取出消息"+mail.toString());
 	}
 
 	@RabbitListener(queues = "topicqueue2")
-	public void displayTopic2(@Headers Map<String, Object> headers, Mail mail) throws JsonProcessingException {
+	public void displayTopic2(Message message, @Headers Map<String, Object> headers, Channel channel, @Payload Mail mail) throws JsonProcessingException {
 		System.out.println(mapper.writeValueAsString(headers));
+		try {
+			System.out.println(message);
+			//告诉服务器收到这条消息 已经被我消费了 可以在队列删掉 这样以后就不会再发了 否则消息服务器以为这条消息没处理掉 后续还会在发
+			channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+			System.out.println("receiver success");
+		} catch (IOException e) {
+			e.printStackTrace();
+			//丢弃这条消息
+			//channel.basicNack(message.getMessageProperties().getDeliveryTag(), false,false);
+			System.out.println("receiver fail");
+		}
 		System.out.println("从topicqueue2取出消息"+mail.toString());
 	}
 
